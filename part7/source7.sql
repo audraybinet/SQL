761aa7ce812be95f7523ae33755465244b15c362
audraybinet <audreybinet@yahoo.fr>
2020-01-22 09:21:38 +0100

    parti7 commenté

--- part7/source7.sql
+++ part7/source7.sql
@@ -1,8 +1,10 @@
 -- exo1 partie7
+-- La commande DELETE en SQL permet de supprimer des lignes dans une table. En utilisant cette commande associé à WHERE il est possible de sélectionner les lignes concernées qui seront supprimées.
 DELETE FROM `languages`
 WHERE `language` = 'HTML';
 
 /* exo2 partie7 */
+-- La commande UPDATE permet d’effectuer des modifications sur des lignes existantes. Très souvent cette commande est utilisée avec WHERE pour spécifier sur quelles lignes doivent porter la ou les modifications.
 
 UPDATE `frameworks`
 SET `version` ='Symfony2'
